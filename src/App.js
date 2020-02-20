bcce5040d231a6dcd3b63f86c01dccf8d8d166c8
Dirga Yasa <cekingx@gmail.com>
2020-02-20 22:29:00 +0800

    Ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import logo from "./pin.svg";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import * as dagangLawar from "./location.json";
+import "./App.css";
+
+export const icon = new Icon({
+  iconUrl: logo,
+  iconSize: [30, 30],
+});
 
 function App() {
+  const [popUp, setPopUp] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Map center={[-8.571246, 115.311178]} zoom={13}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+
+      {dagangLawar.location.map(lawar => (
+        <Marker 
+          key={lawar.id} 
+          position={[lawar.latitude, lawar.longitude]} 
+          onClick={() => {
+            setPopUp(lawar);
+          }}
+          icon={icon}
+        />
+      ))}
+
+      {popUp && (
+        <Popup 
+          position={[popUp.latitude, popUp.longitude]}
+          onClose={() => setPopUp(null)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <h3>{popUp.name}</h3>
+          <br/>
+          <h4>{popUp.address}</h4>
+          <br/>
+          <p>{popUp.description}</p>
+        </Popup>
+      )}
+    </Map>
   );
 }
 
